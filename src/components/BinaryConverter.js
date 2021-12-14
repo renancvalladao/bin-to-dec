a93f8a8585724fd89d3280c666558c9b33b22e4f
Renan Valladão <renancvalladao@gmail.com>
2021-12-13 22:51:07 -0300

    Code formatting

--- src/components/BinaryConverter.js
+++ src/components/BinaryConverter.js
@@ -6,21 +6,21 @@ function BinaryConverter() {
     const [decimalOutput, setDecimalOutput] = useState('')
     const [error, setError] = useState('')
 
-    const convertFromBinToDec = (binaryNum) => {
-        setBinaryInput(binaryNum)
+    const convertFromBinToDec = (binaryString) => {
+        setBinaryInput(binaryString)
 
-        if (binaryNum.length === 0) {
+        if (binaryString.length === 0) {
             setDecimalOutput('')
             setError('')
             return
         }
 
         let result = 0
-        for (let i = 0; i < binaryNum.length; i++) {
-            if (binaryNum[i] === '1') {
-                result += 2 ** (binaryNum.length - i - 1)
+        for (let i = 0; i < binaryString.length; i++) {
+            if (binaryString[i] === '1') {
+                result += 2 ** (binaryString.length - i - 1)
             }
-            else if (binaryNum[i] !== '0') {
+            else if (binaryString[i] !== '0') {
                 setDecimalOutput('')
                 setError("Input must contain only 0's and 1's")
                 return
@@ -41,10 +41,7 @@ function BinaryConverter() {
             <h2 className="name">Binary To Decimal Converter</h2>
             <div className="input">
                 <label htmlFor="binary-input">Binary Number</label>
-                <input type="text" id="binary-input" value={binaryInput} onChange={e => {
-                    convertFromBinToDec(e.target.value)
-                }
-                } onKeyPress={onKeyPress} autoComplete="off" />
+                <input type="text" id="binary-input" value={binaryInput} onChange={e => convertFromBinToDec(e.target.value)} onKeyPress={onKeyPress} autoComplete="off" />
             </div>
             <div className="input">
                 <label>Decimal Number</label>
