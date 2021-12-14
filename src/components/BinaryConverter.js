644fd5f45d7eddfa6aa54c508532b1e26b3ae6b2
Renan Valladão <renancvalladao@gmail.com>
2021-12-13 22:41:55 -0300

    Removing button and fixing css

--- src/components/BinaryConverter.js
+++ src/components/BinaryConverter.js
@@ -6,19 +6,21 @@ function BinaryConverter() {
     const [decimalOutput, setDecimalOutput] = useState('')
     const [error, setError] = useState('')
 
-    const onClick = () => {
-        if (binaryInput.length === 0) {
-            setError('Empty input')
+    const convertFromBinToDec = (binaryNum) => {
+        setBinaryInput(binaryNum)
+
+        if (binaryNum.length === 0) {
             setDecimalOutput('')
+            setError('')
             return
         }
-        
+
         let result = 0
-        for (let i = 0; i < binaryInput.length; i++) {
-            if (binaryInput[i] === '1') {
-                result += 2 ** (binaryInput.length - i - 1)
+        for (let i = 0; i < binaryNum.length; i++) {
+            if (binaryNum[i] === '1') {
+                result += 2 ** (binaryNum.length - i - 1)
             }
-            else if (binaryInput[i] !== '0') {
+            else if (binaryNum[i] !== '0') {
                 setDecimalOutput('')
                 setError("Input must contain only 0's and 1's")
                 return
@@ -39,14 +41,18 @@ function BinaryConverter() {
             <h2 className="name">Binary To Decimal Converter</h2>
             <div className="input">
                 <label htmlFor="binary-input">Binary Number</label>
-                <input type="text" id="binary-input" value={binaryInput} onChange={e => setBinaryInput(e.target.value)} onKeyPress={onKeyPress} autoComplete="off" />
-                <button onClick={onClick}>Convert</button>
+                <input type="text" id="binary-input" value={binaryInput} onChange={e => {
+                    convertFromBinToDec(e.target.value)
+                }
+                } onKeyPress={onKeyPress} autoComplete="off" />
             </div>
             <div className="input">
                 <label>Decimal Number</label>
                 <input type="text" value={decimalOutput} readOnly autoComplete="off" />
             </div>
-            {error ? <p>Error: {error}</p> : ''}
+            <div className="error">
+                {error ? <p>Error: {error}</p> : ''}
+            </div>
         </div>
     )
 }
